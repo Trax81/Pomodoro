237e32a156fae84bfdb7136ae007a3c2d8e2ecf0
Cara Cottingham <boops@Caras-MacBook-Pro.local>
2026-08-16 15:50:50 -0700

    Travis

---
+++ Pomodoro Starter/app.js
@@ -0,0 +1,12 @@
+function startTimer() {
+    let startTime = Date.now()
+    console.log(startTime)
+}
+
+function stopTimer() {
+    console.log('stopTimer()')
+}
+
+function resetTimer() {
+    console.log('resetTimer()')
+}
